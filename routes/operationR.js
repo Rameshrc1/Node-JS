c4ebf841b6dc1d2b1558653ddcfd1d5827cc0f2a
Ramesh Samaraweera <90851929+Rameshrc1@users.noreply.github.com>
2021-11-20 19:19:57 +0530

    add router

--- routes/operationR.js
+++ routes/operationR.js
@@ -6,7 +6,7 @@ router.post('/add',operationsC.validate,operationsC.add);
 router.post('/sub',operationsC.validate,operationsC.sub);
 router.post('/multi',operationsC.validate,operationsC.multi);
 router.post('/div',operationsC.validate,operationsC.div);
-
+router.post('/det',operationsC.validate,operationsC.div);
 
 module.exports = router;
 
